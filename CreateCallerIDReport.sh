31643ed9bdcd36563771c281d29c127c3ad8f635
nowindows <nojunkeplease@gmail.com>
2014-03-31 23:12:41 -0400

    Added CreateCallerIDReport.sh

---
+++ CreateCallerIDReport.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# This shell script generates a callerID.report from a callerID file passed as argument
+# The call count for each call number and callerID is listed, sorted by frequency
+#
+# Usage: ./CreateCallerIDReport.sh callerId.dat
+# 
+# The script assumes chronological entries andthe following format for call entry
+# YYYY-MM-DDThh:mm|callnumber|callerid_string|
+#2014-01-21T11:25|1|Unavailable |
+
+> callerID.Report
+firstDate=$(head -n1 $1 | cut -c-10)
+lastDate=$(tail -n1 $1 | cut -c-10)
+echo "Report By Call Frequency for $1 from $firstDate through $lastDate" >> callerID.Report
+#echo "  Count   Number        Count   CallerID" >> callerID.Report
+
+> callerNumber
+echo "\n  Count   Number" > callerNumber
+cut -f2 -d'|' $1 | sort | uniq -c | sort -b -r -n -t' ' >> callerNumber
+
+> callerID
+echo "\n  Count   CallerID" > callerID
+cut -f3 -d'|' $1 | sort | uniq -c | sort -b -r -n -t' '  >> callerID
+
+> callers
+paste callerNumber callerID  | gawk '{ FS="\t"} ; {printf "%-22s                      %-22s\n", $1, $2}' > callers
+
+#paste callerNumber callerID   > callers
+
+cat callers >> callerID.Report
+rm callerNumber
+rm callerID
+rm callers
+cat callerID.Report
